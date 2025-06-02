fb872c4618b522ff5ea9eb463fc040474150e711
Frosty-Winter-Snowfall <urjaswibanerjee@gmail.com>
2025-06-02 16:01:58 +0530

    P & C registration

--- backend/src/routes.py
+++ backend/src/routes.py
@@ -1,7 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint,request
 from .models import Admin, Session, Parent, Child, Skill, Lesson, LessonHistory, Activity, ActivityHistory, Quiz, QuizHistory, Badge, BadgeHistory
-from . import db
+from .db import db
 from .demoData import createDummyData
+from .controllers import parent_regisc,child_regisc
+
 
 api = Blueprint('api', __name__)
 
@@ -9,3 +11,11 @@ api = Blueprint('api', __name__)
 def dummyData():
     createDummyData()
     return "Dummy data has been created!"
+
+@api.route('/auth/parent_register',methods=['POST'])
+def parent_register():
+    return parent_regisc(request)
+
+@api.route('/auth/children_register',methods=['POST'])
+def child_register():
+    return child_regisc(request)
