bb6d0356fafc8cc5ca38af7deb35e82fdd7cdfce
Yash <yashkc2025@gmail.com>
2025-06-03 09:05:01 +0530

    feat : functions setup frontend

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DemoView from '@/views/DemoView.vue'
+import ParentLoginView from './auth/ParentLoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,19 +16,11 @@ const router = createRouter({
       name: 'demo',
       component: DemoView,
     },
-    // {
-    //   path: '/auth/parent_login',
-    //   name: 'Login as Parent',
-    //   component: ParentLoginView,
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue'),
-    // },
+    {
+      path: '/auth/parent_login',
+      name: 'Login as Parent',
+      component: ParentLoginView,
+    },
   ],
 })
 
