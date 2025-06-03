d79790d32c2690d147bf4bee7b1ff2011e2a7610
Yash <yashkc2025@gmail.com>
2025-06-03 15:35:46 +0530

    feat: added card component

---
+++ frontend/src/components/Card.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #e6e6e6;
+  border-radius: var(--border-radius);
+  /* Testin only */
+  width: 400px;
+}
+</style>
--- frontend/src/views/DemoView.vue
+++ frontend/src/views/DemoView.vue
@@ -1,11 +1,30 @@
 <script setup lang="ts">
+import Card from '@/components/Card.vue';
+import CardItem from '@/components/CardItem.vue';
+
 </script>
 
 <template>
   <main>
-    <p class="text-2xl bg-red-50 ft-head-1 font-black-">I am a heading!</p>
-    <p class="text-lg bg-red-50 ft-delius">This is part of a UI system created for an ed-tech platform. A product loved
-      by both students and educators.
-    </p>
+    <Card>
+      <CardItem>
+        <p class="ft-head-1 text-xl">I am a heading!</p>
+      </CardItem>
+      <CardItem>
+        <p class="text-sm ft-delius">This is part of a UI system created for an ed-tech platform. A product
+          loved
+          by both students and educators.
+        </p>
+      </CardItem>
+    </Card>
   </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
