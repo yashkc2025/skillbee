6e0691bb3cbe8ebf5cb80e8fcd1638a5d0095509
ekta124 <ektabansal7389@gmail.com>
2025-06-03 12:41:08 +0530

    feat: implemented login system

--- backend/src/routes.py
+++ backend/src/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint,request
 from .models import Admin, Session, Parent, Child, Skill, Lesson, LessonHistory, Activity, ActivityHistory, Quiz, QuizHistory, Badge, BadgeHistory
 from .db import db
 from .demoData import createDummyData
-from .controllers import parent_regisc,child_regisc
+from .controllers import parent_regisc,child_regisc,parent_loginc, child_loginc
 
 
 api = Blueprint('api', __name__)
@@ -19,3 +19,11 @@ def parent_register():
 @api.route('/auth/children_register',methods=['POST'])
 def child_register():
     return child_regisc(request)
+
+@api.route('/auth/parent_login',methods=['POST'])
+def parent_login():
+    return parent_loginc(request)
+
+@api.route('/auth/children_login', methods=['POST'])
+def child_login():
+    return child_loginc(request)
