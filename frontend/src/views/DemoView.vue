a5dc5e91259fb99e7d0fe4d16b58232e41f98efc
Yash <yashkc2025@gmail.com>
2025-06-04 19:55:26 +0530

    fix: moved navbar to its own component

--- frontend/src/views/DemoView.vue
+++ frontend/src/views/DemoView.vue
@@ -1,38 +1,10 @@
 <script setup lang="ts">
-import sitemap from "../router/sitemap.json";
-
-const navOptions = [
-  {
-    label: "Dashboard",
-    link: sitemap.child_app.dashboard,
-  },
-  {
-    label: "Lessons",
-    link: sitemap.child_app.lessons,
-  },
-  {
-    label: "Achievements",
-    link: sitemap.child_app.achievements,
-  },
-  {
-    label: "Settings",
-    link: sitemap.child_app.settings,
-  },
-];
+import Navbar from "@/components/child/Navbar.vue";
 </script>
 
 <template>
   <main>
-    <div class="navbar">
-      <section class="logo-parent">
-        <img src="/logo.png" alt="" />
-      </section>
-      <section class="items-parent">
-        <a :href="n.link" class="nav-items" v-for="n in navOptions" :key="n.label">{{
-          n.label
-          }}</a>
-      </section>
-    </div>
+    <Navbar />
   </main>
 </template>
 
@@ -42,49 +14,5 @@ main {
   justify-content: center;
   align-items: center;
   height: 100vh;
-}
-
-.navbar {
-  padding: var(--size-2xs) var(--size-md);
-  border: 1px solid var(--color-border);
-  border-radius: var(--border-radius);
-  display: flex;
-  flex-direction: row;
-  background-color: var(--color-text-light);
-  box-shadow: 0 8px 32px var(--color-border);
-  gap: var(--size-sm);
-}
-
-.items-parent {
-  display: flex;
-  flex-direction: row;
-  gap: 10px;
-}
-
-a {
-  color: var(--color-text-dark);
-  text-decoration: none;
-  padding: var(--size-2xs) var(--size-xs);
-  border-radius: calc(var(--border-radius) / 2);
-  font-size: var(--font-sm);
-  /* border-radius: var(--border-radius); */
-  border: 1px solid none;
-  font-family: "VAGRoundedNext";
-  font-weight: 400;
-}
-
-a:hover {
-  background-color: var(--color-background);
-  border-color: var(--color-border);
-}
-
-.logo-parent {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-img {
-  width: 15px;
 }
 </style>
