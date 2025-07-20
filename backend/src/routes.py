0c7b9e30b66b8e85ca31b2970dab484d8319db33
Urjaswi Banerjee <22f3000877@ds.study.iitm.ac.in>
2025-07-20 14:03:45 +0530

    Feat: Implemented Child Dashboard implemented (#24)
    feat: Implemented Child Dashboard
    
    Co-authored-by: ekta124 <ektabansal7389@gmail.com>

--- backend/src/routes.py
+++ backend/src/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint,request
 from .models import Admin, Session, Parent, Child, Skill, Lesson, LessonHistory, Activity, ActivityHistory, Quiz, QuizHistory, Badge, BadgeHistory
 from .db import db
 from .demoData import createDummyData
-from .controllers import parent_regisc,child_regisc,admin_loginc,parent_loginc, child_loginc
+from .controllers import parent_regisc,child_regisc,admin_loginc,parent_loginc, child_loginc,get_auser,get_child_dashboard_stats,get_user_skill_progress,get_user_badges,get_lesson_quizzes,get_quiz_history,get_curriculums_for_child
 
 
 api = Blueprint('api', __name__)
@@ -41,3 +41,31 @@ def child_login():
     Route for child login
     """
     return child_loginc(request)
+
+@api.route('/auth/get_user',methods=['GET'])
+def get_user():
+    return get_auser()
+
+@api.route('/child_dashboard_stats', methods=['GET'])
+def child_dashboard_stats():
+    return get_child_dashboard_stats()
+
+@api.route('/skill_categories', methods=['GET'])
+def get_skill_categories():
+    return get_user_skill_progress()
+
+@api.route('/user_badges', methods=['GET'])
+def user_badges():
+    return get_user_badges()
+
+@api.route("/api/child/<int:child_id>/curriculum/<int:curriculum_id>/lesson/<int:lesson_id>/quizzes", methods=["GET"])
+def get_quizzes(curriculum_id, lesson_id):
+    return get_lesson_quizzes(curriculum_id=curriculum_id, lesson_id=lesson_id)
+
+@api.route("/api/child/<int:child_id>/quiz/<int:quiz_id>/history", methods=["GET"])
+def get_history(quiz_id):
+    return get_quiz_history(quiz_id=quiz_id)
+
+@api.route('/api/child/<int:child_id>/curriculums', methods=['GET'])
+def get_curr():
+    return get_curriculums_for_child()
