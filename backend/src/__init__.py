9a1c4f4513ff82a15bece15ef333204f6b6d6063
Yash <22f3000472@ds.study.iitm.ac.in>
2025-08-06 23:00:09 +0530

    fix : fix cors error

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -11,7 +11,7 @@ def create_app():
     app.config.from_object(Config)
 
     db.init_app(app)
-    CORS(app)
+    CORS(app, supports_credentials=True, origins=["http://localhost:5173"])
 
     app.register_blueprint(api)
 
